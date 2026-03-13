03cde3154d2a999d37ff9120ccb4c8323ba28230
facelessuser <faceless.shop@gmail.com>
2026-03-13 13:28:21 -0600

    Add Helmlab, Helmgen, and Helmgenlch color spaces (#510)

--- docs/theme/playground-config-3ba60a2e.js
+++ docs/theme/playground-config-3ba60a2e.js
@@ -1,5 +1,13 @@
 var colorNotebook = {
+<<<<<<<< HEAD:docs/theme/playground-config-3ba60a2e.js
     "playgroundWheels": ['micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.8.1-py3-none-any.whl'],
     "notebookWheels": ['pyyaml', 'markdown-3.10.1-py3-none-any.whl', 'pymdown_extensions-10.20-py3-none-any.whl', 'micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.8.1-py3-none-any.whl'],
+|||||||| parent of c12ed5ab (Add Helmlab, Helmgen, and Helmgenlch color spaces (#510)):docs/theme/playground-config-191ce1a5.js
+    "playgroundWheels": ['micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.7-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'markdown-3.10.1-py3-none-any.whl', 'pymdown_extensions-10.20-py3-none-any.whl', 'micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.7-py3-none-any.whl'],
+========
+    "playgroundWheels": ['micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.8-py3-none-any.whl'],
+    "notebookWheels": ['pyyaml', 'markdown-3.10.1-py3-none-any.whl', 'pymdown_extensions-10.20-py3-none-any.whl', 'micropip', 'pygments-2.19.2-py3-none-any.whl', 'coloraide-8.8-py3-none-any.whl'],
+>>>>>>>> c12ed5ab (Add Helmlab, Helmgen, and Helmgenlch color spaces (#510)):docs/theme/playground-config-eec35f0d.js
     "defaultPlayground": "import coloraide\ncoloraide.__version__\nColor('red')"
 }
